57d8171e9d53063103963b9fc124e04df5492203
imygnam <imygnam@gmail.com>
2022-06-21 16:51:07 +0900

    RequiredArgsConstructor 적용

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstproject.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -9,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity //db가 해당 객체를 인식가능
+@RequiredArgsConstructor //기본 생성자 무시하는 것.
 @AllArgsConstructor
 @ToString
 public class Article {
@@ -16,12 +18,11 @@ public class Article {
     @Id //대표값 지정.
     @GeneratedValue //1,2,3 자동생성 어노테이션
     private Long id;
+
     @Column
     private String title;
     @Column
     private String content;
 
-    public Article() {
 
-    }
 }
